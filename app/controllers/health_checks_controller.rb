68d838e5f63706703f3d0f3a7e404af91142d9d8
JunHatanaka <nextseason6@gmail.com>
2024-07-19 20:41:33 +0900

    feat: health_check

---
+++ app/controllers/health_checks_controller.rb
@@ -0,0 +1,8 @@
+class HealthChecksController < ApplicationController
+  # 認証をスキップする場合（API等）
+  # skip_before_action :verify_authenticity_token, only: [:index]
+
+  def index
+    render plain: 'OK', status: :ok
+  end
+end

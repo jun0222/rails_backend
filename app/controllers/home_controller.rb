f2d452e2741cb57c3bb8d49af65b541f950225de
JunHatanaka <nextseason6@gmail.com>
2024-08-23 02:49:13 +0900

    feat: route一覧

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -6,14 +6,31 @@ class HomeController < ApplicationController
     @dotenv_test_value = ENV.fetch('DOTENV_TEST', nil)
   end
 
+  # rubocop:disable all
   def routes
+    excluded_paths = [
+      '/recede_historical_location',
+      '/resume_historical_location',
+      '/refresh_historical_location'
+    ]
+
     @routes = Rails.application.routes.routes.map do |route|
+      # コントローラ名でのフィルタリング
+      next if route.defaults[:controller].nil?
+      next if route.defaults[:controller].include?('rails') ||
+              route.defaults[:controller].include?('active_storage') ||
+              route.defaults[:controller].include?('action_mailbox')
+
+      # パス名でのフィルタリング
+      next if excluded_paths.include?(route.path.spec.to_s.gsub('(.:format)', ''))
+
       {
-        verb: route.verb,
+        verb: route.verb.presence || 'GET',
         path: route.path.spec.to_s.gsub('(.:format)', ''),
         controller: route.defaults[:controller],
-        action: route.defaults[:action]
+        action: route.defaults[:action],
+        name: route.name
       }
-    end
+    end.compact
   end
 end

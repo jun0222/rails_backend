5c5f50e79b562bd79fa14502b268dab0e8e09926
dev63 <47178202+jun0222@users.noreply.github.com>
2025-06-15 18:54:37 +0900

    feat: スプレッドシート機能の追加とルーティング設定 (#8)
    * feat: スプレッドシート機能の追加とルーティング設定
    
    * feat: スプレッドシートIDとAPIキーの変数名を修正
    
    * fix: モックモードの切り替え設定ハードコーディングを削除
    
    * fix: SpreadsheetsControllerのクラス名のスペルミスを修正
    
    * fix: モックモードの切り替え設定を追加

---
+++ app/controllers/spreadsheets_controller.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+# SpreadsheetsController は、クライアントサイドのスプレッドシートアプリケーションを提供します。
+# このコントローラーはビューの表示のみを担当し、データの処理はすべてフロントエンドで行われます。
+class SpreadsheetsController < ApplicationController
+  def index; end
+end

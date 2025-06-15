552cb2781b4e31d6abc6c6d08452482f0d34f16d
JunHatanaka <nextseason6@gmail.com>
2025-06-15 18:48:34 +0900

    fix: SpreadsheetsControllerのクラス名のスペルミスを修正

--- app/controllers/spreadsheets_controller.rb
+++ app/controllers/spreadsheets_controller.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-# SpreadSheetsController は、クライアントサイドのスプレッドシートアプリケーションを提供します。
+# SpreadsheetsController は、クライアントサイドのスプレッドシートアプリケーションを提供します。
 # このコントローラーはビューの表示のみを担当し、データの処理はすべてフロントエンドで行われます。
 class SpreadsheetsController < ApplicationController
   def index; end
